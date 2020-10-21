7c414c325d37daf4fdd61bdc1c0c34fd2a03932c
Ilham DM <dimasilham517@gmail.com>
2020-10-21 09:49:48 +0700

    Praktikum WP2

---
+++ Praktikum1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Dimas Ilham M <br>
+              Saya tinggal di Balapulang <br>
+              Saya suka menonton Anime";
+    }
+}
